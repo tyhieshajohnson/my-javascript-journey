48c223a854e45ac4e32ac2093b564e6ced7d595d
tyhieshajohnson <johnsontyhiesha@gmail.com>
2023-11-08 15:43:48 +0200

    3

--- JS/index.js
+++ JS/index.js
@@ -64,18 +64,33 @@ console.log(numberFour);
 // N.B. READ WITH UNDERSTANDING!!!
 //  1.  Create 3 functions to convert R1000 into dollars, euros and pounds. Take function naming into consideration!
 let convert = 1000;
-let dollar = 54,05;
-let euro = 50,60;
-let aed = 198,50;
+let dollar = 54;
+let euro = 50;
+let aed = 198;
 //  2.  Create a function that takes a value and calculates the amount entered including VAT (15%), amount entered should be R400.
 //      Then display the message in the console, "The amount after tax is ...".
 amount (400)
-function amount(R),
-let amount = R*1.15,
-let rounded amount = a.toFixed(2),
-console.log('The amount after tax is ' $[rounded amount]');
+function amount(R);
+let amount = R*1.15;
+let roundedamount = a.toFixed(2);
+console.log('The amount after tax is' + amount);
 //  3.  Declare 3 variables 1stNum, 2ndNum, 3rdNum and assign values of 8, 20 and 14 respectfully. Now write a function that compares “num1” and “num2” and 
 //      displays the larger value.
+let firstNum = 8;
+let secondNum = 20;
+let thirdNum = 14;
+
+function compareAndDisplayLarger(num1, num2) {
+    if (num1 > num2) {
+      console.log(num1 + " is larger.");
+    } else if (num2 > num1) {
+      console.log(num2 + " is larger.");
+    } else {
+      console.log("Both numbers are equal.");
+    }
+  }
+
+  compareAndDisplayLarger(firstNum, secondNum);
 
 //  4.  Write a function that determines whether “num1” is odd or even and displays the
 //      result. (Tip: remember the modulus operator %)
