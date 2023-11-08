1aa0b359b39b712af5c6847d0c8263c398efca8f
tyhieshajohnson <johnsontyhiesha@gmail.com>
2023-11-08 11:37:20 +0200

    taskOne

---
+++ JS/index.js
@@ -0,0 +1,3 @@
+// A. TASK 1:VARIABLES and OPERATORS
+// 1. Create a variable called “language” and assign it the value “JavaScript”. Then create
+// a variable called “score” and assign it the value “10”.
