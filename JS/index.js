4b4656e817f577f6da5bb8b13ff4e19f45a176c6
tyhieshajohnson <johnsontyhiesha@gmail.com>
2023-11-08 12:15:53 +0200

    2

--- JS/index.js
+++ JS/index.js
@@ -3,3 +3,12 @@
 // a variable called “score” and assign it the value “10”.
 let language = 'javascript';
 let score = 10;
+
+// 2.Create two variables called “length” and “width”. Assign each variable a value of 10 and 5 respectfully.
+let length = 10;
+let width = 5;
+// Use the variables to calculate the area of a rectangle with the length and width specified (remember area = length x width).
+let rectangle = length * width
+// Write the following to the console using CONCATENATION and BACKTICKS: 
+console.log('The area of the rectangle is'.concat(rectangle));
+// “The area of the rectangle is …” where … is the area you calculated.
