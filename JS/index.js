1ff383d4884197cbe7306a38141294e900602e0d
tyhieshajohnson <johnsontyhiesha@gmail.com>
2023-11-08 12:18:59 +0200

    3

--- JS/index.js
+++ JS/index.js
@@ -12,3 +12,9 @@ let rectangle = length * width
 // Write the following to the console using CONCATENATION and BACKTICKS: 
 console.log('The area of the rectangle is'.concat(rectangle));
 // “The area of the rectangle is …” where … is the area you calculated.
+
+// 3.Create two variables called “num1” and “num2.” Assign each variable a value of 10 and 4 respectfully.
+let num1 = 10;
+let num2 = 4;
+// Calculate and display in the console what the remainder is if num1 is divided by num2.
+console.log(num1 / num2);
